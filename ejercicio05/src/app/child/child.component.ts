4b755283b417d58eb049cca90d3d2057d82ed87a
Luciano Daniel Diamand <lucianodiamand@yahoo.com>
2018-10-15 17:45:02 -0300

    Agregado del ejercicio 5

---
+++ ejercicio05/src/app/child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  counter = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    setInterval(() => { this.counter++ }, 1000);
+  }
+
+}
---
+++ ejercicio05/src/app/parent/parent.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  currentCounter;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
