04323dcc17404fba5e0a8a22a71d332694f72f66
Luciano Daniel Diamand <lucianodiamand@yahoo.com>
2018-10-15 15:28:08 -0300

    Agregado del ejercicio 2

---
+++ ejercicio02/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Congratulations!</h1>
+    <h3>You bootstrapped the App Module!</h3>
+  `,
+})
+export class AppComponent { }
