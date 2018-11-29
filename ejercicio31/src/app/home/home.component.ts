d5aad82c7b24967ae62a903185c5deaf5546aa35
Luciano Daniel Diamand <lucianodiamand@yahoo.com>
2018-11-29 20:21:49 -0300

    Agregado del problema

---
+++ ejercicio31/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  theDate = new Date(2035, 3, 1, 14, 15);
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
