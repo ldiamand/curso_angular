46da2409565c968284f0e610f8ef5ec46c1993bc
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2018-11-29 22:45:33 -0300

    Agregado del problema

---
+++ ejercicio33/src/app/child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Input() quantity;
+  squaredQuantity: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ejercicio33/src/app/parent/parent.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  myQuantity = 1;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  updateQuantity() {
+    this.myQuantity++;
+  }
+}
