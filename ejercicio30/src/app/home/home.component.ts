668d7adde39885e41785a74fad2889d4dbb1ae1b
Luciano Diamand <lucianodiamand@gmail.com>
2022-10-04 11:48:47 -0300

    Update to version 14.x

--- ejercicio30/src/app/home/home.component.ts
+++ ejercicio30/src/app/home/home.component.ts
@@ -8,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   myText = "HOLA POR AHI. RENDERIZAME EN MINUSCULAS";
-  
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
