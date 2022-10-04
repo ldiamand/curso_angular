c3f8e3b79f0cf2dded13fe46c5757372dcc231f9
Luciano Diamand <lucianodiamand@gmail.com>
2022-10-04 15:04:37 -0300

    Update to version 14.x

--- ejercicio38/src/app/app.component.spec.ts
+++ ejercicio38/src/app/app.component.spec.ts
@@ -1,27 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  }));
-  it('should create the app', async(() => {
+  });
+
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'ejercicio38'`, async(() => {
+  });
+
+  it(`should have as title 'ejercicio38'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app.title).toEqual('ejercicio38');
-  }));
-  it('should render title in a h1 tag', async(() => {
+  });
+
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ejercicio38!');
-  }));
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ejercicio38 app is running!');
+  });
 });
--- ejercicio38/src/app/parent/parent.component.ts
+++ ejercicio38/src/app/parent/parent.component.ts
@@ -13,3 +13,4 @@ export class ParentComponent implements OnInit {
   }
 
 }
+
